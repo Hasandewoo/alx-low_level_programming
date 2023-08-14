baef73ae86d29dee7c94e0fe4f3f2da106cb553a
Hasandaewoo <hasanhadi27@gmail.com>
2023-08-14 10:46:16 -0700

    positive nor negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
  *Description: 'to print stored in the variable n is positive or negative'
  *Return: alawys 0 (Success)
 */
-<<<<<<< HEAD
 	int main(void)
 {	int n;
 	srand(time(0));
@@ -21,19 +20,4 @@
 		printf("%d is negative", n);
 	printf("\n")
 	return (0);
-=======
-int main(void)
-{
-    int n;
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n == 0)
-        printf("%d is zero\n", n);
-    else
-        printf("%d is negative\n", n);
-    return (0);
-
->>>>>>> dc64ea8131ec8e7ca8a2003d5e95b48b4e0fb567
 }
