dc64ea8131ec8e7ca8a2003d5e95b48b4e0fb567
Hasandewoo <138796317+Hasandewoo@users.noreply.github.com>
2023-08-14 12:48:33 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
+/* more headers goes there */
 /**
  *main - main function
  *Description: 'to print stored in the variable n is positive or negative'
- *Return: alawys 0
+ *Return: alawys 0 (Success)
 */
 int main(void)
-{	int n;
-	srand(time(0));
-n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	printf("%d is positive", n);
-	else if (n == 0)
-	printf("%d is zero", n);
-	else
-	printf("%d is negative", n);
-	printf("\n")
-	return (0); }
+{
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n == 0)
+        printf("%d is zero\n", n);
+    else
+        printf("%d is negative\n", n);
+    return (0);
+
+}
